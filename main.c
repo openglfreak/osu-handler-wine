2dc5508a918ab4690b28219ae8862c8b193d4c2e
Torge Matthies <openglfreak@googlemail.com>
2020-12-14 01:30:08 +0100

    Improve main.c.

--- main.c
+++ main.c
@@ -24,20 +24,21 @@
 #define _POSIX_C_SOURCE 200809L /* openat, readlinkat */
 #define _DEFAULT_SOURCE /* openat, readlinkat */
 
+#include "attrs.h" /* attr_const */
 #include "bool.h" /* bool */
 #include "inline.h" /* inline */
 #include "procdir.h" /* close_procdir, open_procdir, procdir_dirfd,
                         procdir_handle, procdir_next_process */
+#include "static_string.h" /* static_strlen, static_endswith, static_startswith */
 
 #include <assert.h> /* assert */
 #include <dirent.h> /* struct dent */
 #include <errno.h> /* ENOTSUP, errno */
 #include <fcntl.h> /* O_DIRECTORY, O_SEARCH, O_RDONLY, openat */
-#include <libgen.h> /* basename */
 #include <stddef.h> /* size_t, ssize_t */
 #include <stdio.h> /* perror */
 #include <stdlib.h> /* free, malloc, realloc */
-#include <string.h> /* memcmp */
+#include <string.h> /* memcmp, memchr */
 #include <sys/stat.h> /* fstat, struct stat */
 #include <sys/types.h> /* uid_t */
 #include <unistd.h> /* close, execve, getuid, read, readlinkat */
@@ -51,17 +52,14 @@ static inline bool test_uid(int const dirfd)
     return fstat(dirfd, &buf) != -1 && buf.st_uid == our_uid;
 }
 
-#define static_strlen(s) (sizeof(s) - 1)
-
 static inline bool test_comm(int const dirfd)
 {
-#define wanted_comm "osu!.exe\n"
+    static char wanted_comm[] = "osu!.exe\n";
     int fd;
     char buf[static_strlen(wanted_comm)];
     bool b;
 
     fd = openat(dirfd, "comm", O_RDONLY);
-
     if (fd == -1)
         return false;
 
@@ -70,63 +68,62 @@ static inline bool test_comm(int const dirfd)
     close(fd);
 
     return b && memcmp(buf, wanted_comm, static_strlen(wanted_comm)) == 0;
-#undef wanted_comm
 }
 
-#define static_endswith(n, m, e) \
-    ((n) >= static_strlen(e) && \
-    memcmp((m) + ((n) - static_strlen(e)), (e), static_strlen(e)) == 0)
-
-static inline bool test_exe(int const dirfd, char** const out_exe_path)
+static inline char* get_exe_path(int const dirfd, size_t* const path_len)
 {
-#define wanted_exe_name_1 "wine-preloader"
-#define wanted_exe_name_2 "wine64-preloader"
     size_t bufsize;
-    char* buffer,* buffer2;
-    ssize_t link_len;
+    char* buffer;
 
-    bufsize = 128;
+    for (bufsize = 128;;bufsize *= 2)
+    {
+        ssize_t link_len;
 
-    do {
         buffer = (char*)malloc(sizeof(char) * bufsize);
         if (!buffer)
-            return false;
+            break;
 
         link_len = readlinkat(dirfd, "exe", buffer, bufsize);
-
-        if (link_len < 0)
+        if (link_len == -1)
         {
             free(buffer);
-            return false;
+            buffer = 0;
+            break;
         }
 
-        if ((size_t)link_len >= bufsize)
+        if ((size_t)link_len < bufsize)
         {
-            free(buffer);
-            bufsize *= 2;
-            continue;
-        }
-
-        if (static_endswith((size_t)link_len, buffer, "/" wanted_exe_name_1) ||
-            static_endswith((size_t)link_len, buffer, "/" wanted_exe_name_2))
-        {
-            buffer[link_len] = '\0';
+            buffer[(size_t)link_len] = '\0';
+            *path_len = (size_t)link_len;
             break;
         }
 
         free(buffer);
+    }
+
+    return buffer;
+}
+
+static inline bool test_exe(int const dirfd, char** const out_exe_path)
+{
+    char* exe_path;
+    size_t path_len;
+    char* exe_path2;
+
+    exe_path = get_exe_path(dirfd, &path_len);
+    if (!exe_path)
         return false;
-    } while (true);
 
-    buffer2 = (char*)realloc(buffer, sizeof(char) * (link_len + 1));
-    if (buffer2)
-        *out_exe_path = buffer2;
-    else
-        *out_exe_path = buffer;
+    if (!static_endswith((size_t)path_len, exe_path, "/wine-preloader") &&
+        !static_endswith((size_t)path_len, exe_path, "/wine64-preloader"))
+    {
+        free(exe_path);
+        return false;
+    }
 
+    exe_path2 = (char*)realloc(exe_path, sizeof(char) * (path_len + 1));
+    *out_exe_path = exe_path2 ? exe_path2 : exe_path;
     return true;
-#undef wanted_exe_name_1
-#undef wanted_exe_name_2
 }
 
 static inline bool test_dir(int const dirfd, char** const out_exe_path)
@@ -146,116 +143,124 @@ static inline bool test_dir(int const dirfd, char** const out_exe_path)
 static inline bool read_environ(int const dirfd, char** const out_environ,
     size_t* const out_environ_size)
 {
+#define malloc_overhead 32
     int fd;
     size_t bufsize;
-    char* buffer,* buffer2;
+    char* buffer;
     size_t pos;
-
-    bufsize = 4096;
-    pos = 0;
+    bool ret;
 
     fd = openat(dirfd, "environ", O_RDONLY);
     if (fd == -1)
         return false;
 
+    bufsize = 8192 - malloc_overhead;
     buffer = (char*)malloc(sizeof(char) * bufsize);
     if (!buffer)
+    {
+        close(fd);
         return false;
+    }
 
-    do {
+    pos = 0;
+    ret = false;
+    while (true)
+    {
         ssize_t n;
-        n = read(fd, &buffer[pos], bufsize - pos);
-        pos += (size_t)n;
+        char* buffer2;
 
+        n = read(fd, &buffer[pos], bufsize - pos);
         if (n < 0)
         {
             free(buffer);
-            return false;
+            break;
         }
 
-        if (pos < bufsize)
+        if (n == 0)
         {
-            n = read(fd, &buffer[pos], bufsize - pos);
-            pos += (size_t)n;
-
-            if (n < 0)
-            {
-                free(buffer);
-                return false;
-            }
-
-            if (n == 0)
-                break;
+            buffer2 = (char*)realloc(buffer, sizeof(char) * pos);
+            *out_environ = buffer2 ? buffer2 : buffer;
+            *out_environ_size = pos;
+            ret = true;
+            break;
         }
 
+        pos += (size_t)n;
         if (pos >= bufsize)
         {
-            bufsize *= 2;
+            bufsize = (bufsize + malloc_overhead) * 2 - malloc_overhead;
             buffer2 = (char*)realloc(buffer, sizeof(char) * bufsize);
             if (!buffer2)
             {
                 free(buffer);
-                return false;
+                break;
             }
             buffer = buffer2;
         }
-    } while (true);
+    }
 
-    buffer2 = (char*)realloc(buffer, sizeof(char) * pos);
-    if (buffer2)
-        *out_environ = buffer2;
-    else
-        *out_environ = buffer;
-    *out_environ_size = pos;
-
-    return true;
+    close(fd);
+    return ret;
+#undef malloc_overhead
 }
 
-#define static_startswith(n, m, e) \
-    ((n) >= static_strlen(e) && memcmp((m), (e), static_strlen(e)) == 0)
+static inline attr_const size_t count_envvars(char const* environ,
+    char const* const environ_end)
+{
+    size_t count = 0;
+    while ((environ = (char const*)memchr(environ, '\0', environ_end - environ)))
+    {
+        ++environ;
+        ++count;
+    }
+    return count;
+}
 
-/* Don't look at this function too closely. */
 static inline bool construct_envp(char* environ,
     size_t const environ_size, char*** const out_envp)
 {
     char* const environ_end = &environ[environ_size];
+    size_t n;
     char** envp;
     char* p;
     char** p2;
-    size_t n;
 
-    p = environ;
-    n = 0;
-    for (; p != environ_end; ++p)
-        if (*p == '\0')
-            ++n;
+    n = count_envvars(environ, environ_end);
 
     envp = (char**)malloc(sizeof(char*) * n);
     if (!envp)
         return false;
 
-    p = environ;
     p2 = envp;
-    while (p != environ_end)
+    for (p = environ; (p = (char*)memchr(p, '\0', environ_end - p)); environ = p)
     {
-        while (p != environ_end && *p++ != '\0');
+        ++p;
 
-        if (!static_startswith((size_t)(p - environ), environ,
+        if (static_startswith((size_t)(p - environ), environ,
                 "WINELOADERNOEXEC="))
-        {
-            *p2++ = environ;
-            assert(n-- > 0);
-        }
+            continue;
 
-        environ = p;
+        *p2++ = environ;
+        assert(n-- > 0);
     }
+    *p2++ = 0;
 
-    *out_envp = envp;
+    p2 = (char**)realloc(envp, sizeof(char*) * (p2 - envp));
+    *out_envp = p2 ? p2 : envp;
     return true;
 }
 
-static inline void handle_dir(int const proc_dirfd,
-    struct dirent const* const dent, char* argv[])
+/* Because POSIX says basename(3) may write to the input string... */
+static inline attr_const char const* basename_n(char const* const path,
+    size_t const length)
+{
+    char const* ptr = path + length;
+    while (ptr > path && *--ptr != '/');
+    return ptr;
+}
+
+static inline int handle_dir(int const proc_dirfd,
+    struct dirent const* const dent, char* argv[], bool* const out_error)
 {
     int dirfd;
     char* exe_path;
@@ -263,6 +268,7 @@ static inline void handle_dir(int const proc_dirfd,
     bool b;
     size_t environ_size;
     char** envp;
+    size_t exe_path_length;
 
 #ifdef O_SEARCH
     dirfd = openat(proc_dirfd, dent->d_name, O_SEARCH | O_DIRECTORY);
@@ -270,7 +276,7 @@ static inline void handle_dir(int const proc_dirfd,
     dirfd = openat(proc_dirfd, dent->d_name, O_RDONLY | O_DIRECTORY);
 #endif
     if (dirfd == -1)
-        return;
+        return (errno == ESRCH || errno == ENOENT) ? 0 : errno;
 
     b = test_dir(dirfd, &exe_path);
 
@@ -281,11 +287,16 @@ static inline void handle_dir(int const proc_dirfd,
     b = b && construct_envp(environ, environ_size, &envp);
 
     if (!b)
-        return;
+    {
+        *out_error = true;
+        return 0;
+    }
 
-    exe_path[strlen(exe_path) - static_strlen("-preloader")] = '\0';
-    argv[0] = basename(exe_path);
+    exe_path_length = strlen(exe_path);
+    exe_path[exe_path_length -= static_strlen("-preloader")] = '\0';
+    argv[0] = (char*)basename_n(exe_path, exe_path_length);
     execve(exe_path, argv, envp);
+    return errno;
 }
 
 static int handle_error(int error)
@@ -294,16 +305,16 @@ static int handle_error(int error)
     perror("error");
 
     error &= 0xFF;
-    error |= !!error;
-    return error;
+    return error ? error : 1;
 }
 
 int main(int argc, char* argv[])
 {
     int error;
     procdir_handle pdhandle;
-    struct dirent* dent;
     int dirfd;
+    struct dirent* dent;
+    bool exit_loop;
 
     (void)argc;
 
@@ -315,14 +326,15 @@ int main(int argc, char* argv[])
     if ((dirfd = procdir_dirfd(pdhandle)) == -1)
         return handle_error(ENOTSUP);
 
-    while ((error = procdir_next_process(pdhandle, &dent)) == 0 && dent)
-        handle_dir(dirfd, dent, argv);
+    exit_loop = false;
+    do {
+        if ((error = procdir_next_process(pdhandle, &dent)) != 0 || dent)
+            break;
+        if ((error = handle_dir(dirfd, dent, argv, &exit_loop)) != 0)
+            break;
+    } while (!exit_loop);
 
     close_procdir(pdhandle);
 
-    if (error != 0)
-        return handle_error(error);
-
-    return 0;
+    return error != 0 ? handle_error(error) : 0;
 }
-
